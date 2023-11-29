130c250b77a9057de7d2a12805eccbbfaef16672
Lachicagladiadora <200379@unsaac.edu.pe>
2023-11-29 17:37:15 -0500

    feat(cicle): add expand

--- src/components/Cicle.tsx
+++ src/components/Cicle.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import { LogDayInputs } from "./LogDayInputs";
-import { faAngleLeft, faGear, faPlus } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAngleLeft,
+  faAngleRight,
+  faGear,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
 import { Button } from "./Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -14,6 +19,7 @@ export const Cicle = ({}: CicleProps) => {
   const [logDays, setLogDays] = useState<LogDay[]>([
     { repetition: 0, weight: 0 },
   ]);
+  const [expand, setExpand] = useState(false);
 
   const onChangeRepetitionsByIdx = (idx: number) => (newValue: number) =>
     setLogDays((prev) =>
@@ -39,27 +45,42 @@ export const Cicle = ({}: CicleProps) => {
           <Button onClick={onAddNewLogDay}>
             <FontAwesomeIcon icon={faPlus} />
           </Button>
-          <FontAwesomeIcon icon={faGear} />
-          <FontAwesomeIcon icon={faAngleLeft} />
+          <Button>
+            <FontAwesomeIcon icon={faGear} />
+          </Button>
+          <Button onClick={() => setExpand((prev) => !prev)}>
+            <FontAwesomeIcon
+              icon={faAngleRight}
+              className={`${
+                expand
+                  ? "rotate-180 transition-[1s]"
+                  : "rotate-0 transition-[1s]"
+              }`}
+            />
+          </Button>
         </div>
       </div>
-      <div className="flex flex-col gap-2 font-bold p-4 justify-center">
-        <span className="h-8">Weigth:</span>
-        <span className="h-8">Repetitions:</span>
-        <span className="h-8">RM:</span>
-      </div>
-      <div className="flex gap-3 overflow-x-scroll p-4">
-        {logDays.map((cur, idx) => (
-          <LogDayInputs
-            key={idx}
-            weight={cur.weight}
-            repetitions={cur.repetition}
-            onChangeRepetitions={onChangeRepetitionsByIdx(idx)}
-            onChangeWeight={onChangeWeightByIdx(idx)}
-            wrapperProps={{ style: { minWidth: "80px" } }}
-          />
-        ))}
-      </div>
+      {expand && (
+        <>
+          <div className="flex flex-col gap-2 font-bold p-4 justify-center">
+            <span className="h-8">Weigth:</span>
+            <span className="h-8">Repetitions:</span>
+            <span className="h-8">RM:</span>
+          </div>
+          <div className="flex gap-3 overflow-x-auto p-4">
+            {logDays.map((cur, idx) => (
+              <LogDayInputs
+                key={idx}
+                weight={cur.weight}
+                repetitions={cur.repetition}
+                onChangeRepetitions={onChangeRepetitionsByIdx(idx)}
+                onChangeWeight={onChangeWeightByIdx(idx)}
+                wrapperProps={{ style: { minWidth: "80px" } }}
+              />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
