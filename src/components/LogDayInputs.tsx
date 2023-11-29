8ae543654b0ba0b6b94e1de8f88601bf8dcc9152
elVengador <dev.jimynicanor@gmail.com>
2023-11-28 22:59:46 -0500

    implement LogDays component

--- src/components/LogDayInputs.tsx
+++ src/components/LogDayInputs.tsx
@@ -2,21 +2,23 @@ import React from "react";
 import { Input } from "./Input";
 
 type LogDayInputsProps = {
+  weight:number,
+  repetitions:number,
   onChangeWeight: (newWeight: number) => void;
-  onChangeRepetitions: () => void;
-  onChangeRM: () => void;
+  onChangeRepetitions: (newRepetitions:number) => void;
 };
 
 export const LogDayInputs = ({
+  weight,
+  repetitions,
   onChangeWeight,
   onChangeRepetitions,
-  onChangeRM,
 }: LogDayInputsProps) => {
   return (
     <div className="flex flex-col gap-2 w-full">
-      <Input onChange={(e) => onChangeWeight(Number(e.target.value))} />
-      {/* <Input onChange={onChangeRepetitions}/> */}
-      {/* <Input onChange={onChangeRM}/> */}
+      <Input type="number" value={weight} onChange={(e) => onChangeWeight(Number(e.target.value))} />
+      <Input type="number" value={repetitions} onChange={e=>onChangeRepetitions(Number(e.target.value))}/>
+      <Input disabled/>
     </div>
   );
 };
